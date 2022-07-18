c64ecd6b24e9bca9686b5d506b0cbf82cf7cbfe1
shelsonx <shelsonx@gmail.com>
2022-07-18 20:32:03 +0200

    event examples

--- first_example.c
+++ first_example.c
@@ -1,27 +1,60 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <mlx.h>
+#include <X11/keysym.h>
+#include <X11/X.h>
 
 # define WINDOW_WIDTH 600
 # define WINDOW_HEIGHT 300
 # define MLX_ERROR 1
 
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_data;
+
+int	handle_no_event(void *data)
+{
+	/* This function needs to exist, but it is useless for the moment*/
+	return (0);
+}
+
+int	handle_keypress(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		printf("Keypress: %d\n", keysym);
+	return (0);
+}
+
+int handle_keyrelease(int keysym, void *data)
+{
+	printf("Keyrelease: %d\n", keysym);
+	return (0);
+}
+
 int main(void)
 {
-	void *mlx_ptr;
-	void *win_ptr;
+	t_data	data;
 
-	mlx_ptr = mlx_init();
-	if(mlx_ptr == NULL)
+	data.mlx_ptr = mlx_init();
+	if(data.mlx_ptr == NULL)
 		return (MLX_ERROR);
-	win_ptr = mlx_new_window(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT,  "My first window");
-	if (win_ptr == NULL)
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT,  "My first window");
+	if (data.win_ptr == NULL)
 	{
-		free(win_ptr);
+		free(data.win_ptr);
 		return (MLX_ERROR);
 	}
-	while (1)
-		;
-	mlx_destroy_window(mlx_ptr, win_ptr);
-	mlx_destroy_display(mlx_ptr);
-	free(mlx_ptr);
+
+	/* Setup hooks*/
+	mlx_loop_hook(data.mlx_ptr, &handle_no_event, &data);
+	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
+	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &handle_keyrelease, &data);
+
+	mlx_loop(data.mlx_ptr);
+	
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
 }
