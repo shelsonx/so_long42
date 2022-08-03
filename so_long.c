120715337fc0f4988b6d08ec4d435d70ad293248
shelsonx <shelsonx@gmail.com>
2022-08-03 03:14:09 +0200

    add validate map

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: sjhony-x <sjhony-x@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 15:40:52 by sjhony-x          #+#    #+#             */
-/*   Updated: 2022/08/02 22:27:58 by sjhony-x         ###   ########.fr       */
+/*   Updated: 2022/08/03 02:44:55 by sjhony-x         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@ int	main(int argc, char **argv)
 {
 	t_game game;
 	game.map = generate_map(argv[argc - 1]);
+	if (!is_valid_map(game.map))
+	{
+		ft_putendl_fd("Map inválido!", 1);
+		free_tab(game.map);
+		return (1);
+	}
 	game.mlx_ptr = mlx_init();
 	if (game.mlx_ptr == NULL)
 		return (MLX_ERROR);
