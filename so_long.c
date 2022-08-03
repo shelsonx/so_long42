9824b0270a6fd09e9013fb25b3b73d2fc8745a8f
shelsonx <shelsonx@gmail.com>
2022-08-03 14:47:45 +0200

    creare function close_win

--- so_long.c
+++ so_long.c
@@ -6,11 +6,22 @@
 /*   By: sjhony-x <sjhony-x@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 15:40:52 by sjhony-x          #+#    #+#             */
-/*   Updated: 2022/08/03 05:50:43 by sjhony-x         ###   ########.fr       */
+/*   Updated: 2022/08/03 14:24:41 by sjhony-x         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+int	close_win(int key, t_game *game)
+{
+	if (key == KEY_ESC)
+	{
+		mlx_destroy_window(game->mlx_ptr, game->window.win_ptr);
+		game->window.win_ptr = NULL;
+		exit(0);
+	}
+	return (0);
+}
 
 int	main(int argc, char **argv)
 {
@@ -28,6 +39,7 @@ int	main(int argc, char **argv)
 	init_window(&game);
 	load_images(&game);
 	mlx_loop_hook(game.mlx_ptr, &render_map, &game);
+	mlx_key_hook(game.window.win_ptr, &close_win, &game);
 	mlx_loop(game.mlx_ptr);
 	mlx_destroy_window(game.mlx_ptr, game.window.win_ptr);
 	free(game.mlx_ptr);
