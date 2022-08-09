bdd7b68d01f23a5f11de62acb289b772861c52b3
shelsonx <shelsonx@gmail.com>
2022-08-09 16:26:44 +0200

    alter enemy for tests

--- sources_bonus/animation_bonus.c
+++ sources_bonus/animation_bonus.c
@@ -6,15 +6,31 @@
 /*   By: sjhony-x <sjhony-x@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 18:33:53 by sjhony-x          #+#    #+#             */
-/*   Updated: 2022/08/09 02:59:32 by sjhony-x         ###   ########.fr       */
+/*   Updated: 2022/08/09 16:25:50 by sjhony-x         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long_bonus.h"
 
+#include <time.h> //apagar depois
+  
+void delay(int number_of_seconds)
+{
+    // Converting time into milli_seconds
+    int milli_seconds = 1000 * number_of_seconds;
+  
+    // Storing start time
+    clock_t start_time = clock();
+  
+    // looping till required time is not achieved
+    while (clock() < start_time + milli_seconds)
+        ;
+}
+
 
 void	swap_bomb(t_game *game, char *path)
 {
+	
 	mlx_destroy_image(game->mlx_ptr, game->image_bonus.bomb.addr);
 	game->image_bonus.bomb.addr = mlx_xpm_file_to_image(
 			game->mlx_ptr, path,
@@ -46,11 +62,58 @@ void	swap_play_for_bomb(t_game *game, char coord, int move)
 	game->map[game->image_bonus.bomb.x][game->image_bonus.bomb.y] = 'B';
 }
 
+void	left_animation(t_game *game)
+{
+	if (game->map[game->image_bonus.bomb.x][game->image_bonus.bomb.y - 1] == 'P')
+		game_over(game);
+	else if (game->map[game->image_bonus.bomb.x][game->image_bonus.bomb.y - 1] == '0')
+	{
+		swap_bomb(game, PATH_IMG_BOMB);
+		swap_play_for_bomb(game, 'y', DECREMENT);
+	}
+}
+
+void	right_animation(t_game *game)
+{
+	if (game->map[game->image_bonus.bomb.x][game->image_bonus.bomb.y + 1] == 'P')
+		game_over(game);
+	else if (game->map[game->image_bonus.bomb.x][game->image_bonus.bomb.y + 1] == '0')
+	{
+		swap_bomb(game, PATH_IMG_BOMB);
+		swap_play_for_bomb(game, 'y', INCREMENT);
+	}
+}
+
 int	animation(t_game *game)
 {
-	swap_bomb(game, PATH_IMG_BOMB_1);
-	render_map(game);
+	/* if (game->map[game->image_bonus.bomb.x][game->image_bonus.bomb.y - 1] == '0' || 
+		game->map[game->image_bonus.bomb.x][game->image_bonus.bomb.y - 1] == 'P')
+	{
+		delay(10);
+		left_animation(game);
+		swap_bomb(game, PATH_IMG_BOMB_1);
+		render_map(game);
+		swap_bomb(game, PATH_IMG_BOMB);
+		render_map(game);
+	}
+	else if (game->map[game->image_bonus.bomb.x][game->image_bonus.bomb.y + 1] == '0' ||
+			game->map[game->image_bonus.bomb.x][game->image_bonus.bomb.y + 1] == 'P')
+	{
+		while (game->map[game->image_bonus.bomb.x][game->image_bonus.bomb.y + 1] == '0')
+		{
+			delay(10);
+			right_animation(game);
+			swap_bomb(game, PATH_IMG_BOMB_1);
+			render_map(game);
+			swap_bomb(game, PATH_IMG_BOMB);
+			render_map(game);
+			if (game->map[game->image_bonus.bomb.x][game->image_bonus.bomb.y + 1] == 'P')
+				game_over(game);
+		}
+	} */
 	swap_bomb(game, PATH_IMG_BOMB);
+	render_map(game);
+	swap_bomb(game, PATH_IMG_BOMB_1);
 	render_map(game);
 	return (0);
 }
